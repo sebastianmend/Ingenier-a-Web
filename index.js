2016f8bf6ce9ee1d44dfd6219bfc7ac76135d265
sebastianmend <sebastianfelipemendietalima@gmail.com>
2024-10-23 15:13:36 -0500

    Tested

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// Renderizamos la aplicación principal en el div con id 'root'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
